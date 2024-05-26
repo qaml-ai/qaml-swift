4a8b16dffa2e21185c1970f6cd017598f9ab6a3d
Miguel Salinas <mikeys@me.com>
2024-05-26 12:12:39 -0700

    remove launch test

--- qaml-test-harness/qaml-test-harnessUITests/qaml_sanity_tests.swift
+++ qaml-test-harness/qaml-test-harnessUITests/qaml_sanity_tests.swift
@@ -66,12 +66,4 @@ final class qaml_sanity_tests: XCTestCase {
         q.assertCondition("The screenshot is of YouTube Shorts. It shows a video or image. The bottom section of the screen shows the video's title, the creator's username. On the right there are options to like, dislike, comment, and share.")
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
